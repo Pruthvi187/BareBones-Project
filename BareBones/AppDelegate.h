1f3d059c35585f48a33373399c1e5a50196a515c
Henly <pruthvi@au1.ibm.com>
2018-05-18 09:21:43 +1000

    New project.

---
+++ BareBones/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BareBones
+//
+//  Created by Pruthvi Henly Onkar on 18/5/18.
+//  Copyright © 2018 Pruthvi Henly Onkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BareBones/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BareBones
+//
+//  Created by Pruthvi Henly Onkar on 18/5/18.
+//  Copyright © 2018 Pruthvi Henly Onkar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
