65739bd7c9e4f78c3bdb0c1db0b5ba924f2909e5
Alexander Jaroszewicz <alex.jaro@au1.ibm.com>
2018-05-21 17:20:32 +1000

    adding the settings page

---
+++ BareBones/View Controllers/New Group/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  BareBones
+//
+//  Created by Alexander Jaroszewicz on 21/5/18.
+//  Copyright © 2018 Pruthvi Henly Onkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
