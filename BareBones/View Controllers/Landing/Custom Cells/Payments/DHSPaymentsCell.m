7987359113ab32aa7bd8f0f9612b07652e9c8cd9
Ian Craigmile <ian@reezen.com>
2018-05-21 14:29:09 +1000

    Shadows on table view groups

--- BareBones/View Controllers/Landing/Custom Cells/Payments/DHSPaymentsCell.m
+++ BareBones/View Controllers/Landing/Custom Cells/Payments/DHSPaymentsCell.m
@@ -6,19 +6,38 @@
 //  Copyright © 2018 Pruthvi Henly Onkar. All rights reserved.
 //
 
+#import <QuartzCore/QuartzCore.h>
+
 #import "DHSPaymentsCell.h"
 
 @implementation DHSPaymentsCell
 
 - (void)awakeFromNib {
     [super awakeFromNib];
-    // Initialization code
+
+    // [self configureBackingView];
 }
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
+//- (void)configureBackingView {
+//    CALayer *layer = self.backingView.layer;
+//    layer.cornerRadius = 15.0f;
+//    layer.masksToBounds = NO;
+//
+//    layer.shadowOffset = CGSizeMake(0, 3);
+//    layer.shadowColor = [[UIColor blackColor] CGColor]; // TODO
+//    layer.shadowRadius = 2.0f;
+//    layer.shadowOpacity = 0.35f;
+//    layer.shadowPath = [[UIBezierPath bezierPathWithRoundedRect:layer.bounds cornerRadius:layer.cornerRadius] CGPath];
+//
+//    CGColorRef bColor = self.backingView.backgroundColor.CGColor;
+//    self.backingView.backgroundColor = nil;
+//    layer.backgroundColor = bColor;
+//}
 
-    // Configure the view for the selected state
-}
+//- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+//    [super setSelected:selected animated:animated];
+//
+//    // Configure the view for the selected state
+//}
 
 @end
--- BareBones/View Controllers/landing/Delegates/DHSLandingListDelegate.m
+++ BareBones/View Controllers/landing/Delegates/DHSLandingListDelegate.m
@@ -70,7 +70,6 @@ typedef NS_ENUM(NSInteger, SectionType) {
         case paymentsSection:
             reuseIdentifier = @"paymentscell";
             cell = (DHSPaymentsCell *)[tableView dequeueReusableCellWithIdentifier:reuseIdentifier forIndexPath:indexPath];
-
             break;
         case tasksSection:
             reuseIdentifier = @"taskcell";
@@ -92,7 +91,7 @@ typedef NS_ENUM(NSInteger, SectionType) {
     NSInteger hgt = 0;
     switch (indexPath.section) {
         case paymentsSection:
-            hgt = 150;
+            hgt = 130;
             break;
         case tasksSection:
             hgt = 80;
@@ -105,6 +104,81 @@ typedef NS_ENUM(NSInteger, SectionType) {
             break;
     }
     return hgt;
+}
+
+//
+// Programmatically create a view to be used as the background view of the cell
+// Round the corners
+// Draw a shadow
+//
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+    CGFloat cornerRadius = 10.0;
+    CGFloat leftAndRightMargin = 10.0f;
+    
+    cell.backgroundColor = UIColor.clearColor;
+    
+    // create a layer for a "background view" for the cell
+    CAShapeLayer *newLayer = [[CAShapeLayer alloc] init];
+    CGMutablePathRef pathRef = CGPathCreateMutable();
+    CGRect indentedBounds = CGRectInset(cell.bounds, leftAndRightMargin, 0.0f); // space left and right
+    BOOL addLine = NO;
+    
+    if (indexPath.row == 0 && indexPath.row == [tableView numberOfRowsInSection:indexPath.section]-1) { // a section with 1 row
+        CGPathAddRoundedRect(pathRef, nil, indentedBounds, cornerRadius, cornerRadius);
+    }
+    else
+    if (indexPath.row == 0) { // top cell
+        CGPathMoveToPoint(pathRef, nil, CGRectGetMinX(indentedBounds), CGRectGetMaxY(indentedBounds));
+        CGPathAddArcToPoint(pathRef, nil, CGRectGetMinX(indentedBounds), CGRectGetMinY(indentedBounds), CGRectGetMidX(indentedBounds), CGRectGetMinY(indentedBounds), cornerRadius);
+        CGPathAddArcToPoint(pathRef, nil, CGRectGetMaxX(indentedBounds), CGRectGetMinY(indentedBounds), CGRectGetMaxX(indentedBounds), CGRectGetMidY(indentedBounds), cornerRadius);
+        CGPathAddLineToPoint(pathRef, nil, CGRectGetMaxX(indentedBounds), CGRectGetMaxY(indentedBounds));
+        addLine = YES;
+    }
+    else
+    if (indexPath.row == [tableView numberOfRowsInSection:indexPath.section]-1) { // bottom cell
+        CGPathMoveToPoint(pathRef, nil, CGRectGetMinX(indentedBounds), CGRectGetMinY(indentedBounds));
+        CGPathAddArcToPoint(pathRef, nil, CGRectGetMinX(indentedBounds), CGRectGetMaxY(indentedBounds), CGRectGetMidX(indentedBounds), CGRectGetMaxY(indentedBounds), cornerRadius);
+        CGPathAddArcToPoint(pathRef, nil, CGRectGetMaxX(indentedBounds), CGRectGetMaxY(indentedBounds), CGRectGetMaxX(indentedBounds), CGRectGetMidY(indentedBounds), cornerRadius);
+        CGPathAddLineToPoint(pathRef, nil, CGRectGetMaxX(indentedBounds), CGRectGetMinY(indentedBounds));
+    }
+    else { // a cell in the middle somewhere
+        CGPathAddRect(pathRef, nil, indentedBounds);
+        addLine = YES;
+    }
+    newLayer.path = pathRef;
+    CFRelease(pathRef);
+    newLayer.fillColor = [UIColor colorWithWhite:1.0f alpha:0.8f].CGColor;
+    
+    // create the separator
+    if (addLine == YES) {
+        CALayer *lineLayer = [[CALayer alloc] init];
+        CGFloat lineHeight = (1.0f / [UIScreen mainScreen].scale);
+        lineLayer.frame = CGRectMake(CGRectGetMinX(indentedBounds)+5, indentedBounds.size.height-lineHeight, indentedBounds.size.width-5, lineHeight);
+        lineLayer.backgroundColor = tableView.separatorColor.CGColor;
+        [newLayer addSublayer:lineLayer];
+    }
+    
+    // create a new "background view" using this programmatically created layer
+    UIView *roundedShadowView = [[UIView alloc] initWithFrame:indentedBounds];
+    [roundedShadowView.layer insertSublayer:newLayer atIndex:0];
+    roundedShadowView.backgroundColor = UIColor.clearColor;
+        
+//        // shadow: start
+//        CALayer *layer = roundedShadowView.layer;
+//        layer.masksToBounds = NO;
+//        
+//        layer.shadowOffset = CGSizeMake(0, 3);
+//        layer.shadowColor = [[UIColor blackColor] CGColor]; // TODO
+//        layer.shadowRadius = 2.0f;
+//        layer.shadowOpacity = 0.35f;
+//        layer.shadowPath = [[UIBezierPath bezierPathWithRoundedRect:indentedBounds cornerRadius:cornerRadius] CGPath];
+//        
+//        CGColorRef bColor = roundedShadowView.backgroundColor.CGColor;
+//        roundedShadowView.backgroundColor = [UIColor whiteColor]; // @IC nil;
+//        layer.backgroundColor = bColor;
+//        // shadow: end
+        
+    cell.backgroundView = roundedShadowView;
 }
 
 @end
