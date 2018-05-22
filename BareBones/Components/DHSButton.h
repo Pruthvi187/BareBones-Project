1bb39b74fb35cf3b573a1f1d86c3ff22e449cc41
Alexander Jaroszewicz <alex.jaro@au1.ibm.com>
2018-05-22 10:14:53 +1000

    adding button file

---
+++ BareBones/Components/DHSButton.h
@@ -0,0 +1,15 @@
+//
+//  DHSButton.h
+//  BareBones
+//
+//  Created by Alexander Jaroszewicz on 21/5/18.
+//  Copyright © 2018 Pruthvi Henly Onkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+#import "DHSColorPool.h"
+
+
+@interface DHSButton : UIButton
+@end
