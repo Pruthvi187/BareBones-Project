8803ae2a46f06db72bff11f070250157d1cf8922
Ian Craigmile <ian@reezen.com>
2018-05-22 10:00:50 +1000

    Tab bar icons hooked up

--- BareBones/Main Tabs/DHSMainTabsTabBarController.m
+++ BareBones/Main Tabs/DHSMainTabsTabBarController.m
@@ -25,21 +25,44 @@
     self.delegate = self.mainTabsDelegate;
     
     DHSLandingVC *landingVC = [DHSLandingVC new];
-    DHSOfficeLocatorVC *officeLocatorVC = [DHSOfficeLocatorVC new];
-    
+    DHSOfficeLocatorVC *profileVC = [DHSOfficeLocatorVC new];
+    DHSOfficeLocatorVC *inboxVC = [DHSOfficeLocatorVC new];
+    DHSOfficeLocatorVC *assistantVC = [DHSOfficeLocatorVC new];
+    DHSOfficeLocatorVC *moreVC = [DHSOfficeLocatorVC new];
+
     UINavigationController *landingNC = [[UINavigationController alloc] initWithRootViewController:landingVC];
     landingNC.title = @"Home";
+    landingNC.tabBarItem.image = [UIImage imageNamed:@"home"];
 
-    UINavigationController *officeLocatorNC = [[UINavigationController alloc] initWithRootViewController:officeLocatorVC];
-    officeLocatorNC.title = @"Office";
+    UINavigationController *profileNC = [[UINavigationController alloc] initWithRootViewController:profileVC];
+    profileNC.title = @"Profile";
+    profileNC.tabBarItem.image = [UIImage imageNamed:@"user"];
     
-
+    UINavigationController *inboxNC = [[UINavigationController alloc] initWithRootViewController:inboxVC];
+    inboxNC.title = @"Inbox";
+    inboxNC.tabBarItem.image = [UIImage imageNamed:@"envelope"];
+    
+    UINavigationController *assistantNC = [[UINavigationController alloc] initWithRootViewController:assistantVC];
+    assistantNC.title = @"Assistant";
+    assistantNC.tabBarItem.image = [UIImage imageNamed:@"comments"];
+    
+    UINavigationController *moreNC = [[UINavigationController alloc] initWithRootViewController:moreVC];
+    moreNC.title = @"More";
+    moreNC.tabBarItem.image = [UIImage imageNamed:@"bars"];
+    
     NSMutableArray *controllerArray = [NSMutableArray arrayWithCapacity:5];
     
     [controllerArray insertObject:landingNC atIndex:0];
-    [controllerArray insertObject:officeLocatorNC atIndex:1];
+    [controllerArray insertObject:profileNC atIndex:1];
+    [controllerArray insertObject:inboxNC atIndex:2];
+    [controllerArray insertObject:assistantNC atIndex:3];
+    [controllerArray insertObject:moreNC atIndex:4];
 
     self.viewControllers = controllerArray;
+}
+
+- (void)setTabBarItems {
+    
 }
 
 @end
